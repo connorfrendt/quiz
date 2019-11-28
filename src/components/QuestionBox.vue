b009e6d22bc5e633f366f95f8e088a6491c3dc7f
Connor Frendt <connor.frendt@pollinate.com>
2019-11-27 16:28:18 -0800

    working on quiz project

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="question-box-container">
+    <b-jumbotron>
+      <template slot="lead">
+        {{ currentQuestion.question }}
+      </template>
+      <hr class="my-4" />
+      
+      <p></p>
+
+      <b-button variant="primary" href='#'>Submit</b-button>
+      <b-button @click="next" variant="success" href='#'>Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function
+  },
+  computed: {
+    answers() {
+      let answers = [...currentQuestion.incorrect_answers];
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
